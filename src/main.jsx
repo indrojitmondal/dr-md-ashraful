edb95801b35a449c2e3293db4bf245b9e0a53e20
indroji-mondal <indro.cse.bu@gmail.com>
2026-05-12 18:54:10 +0600

    TailwindCSS and DaisyUI setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,3 @@
-// import * as React from "react";
-// import * as ReactDOM from "react-dom/client";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import "./index.css";
-// import { router } from "./layouts/routes/Routes";
-// import { Helmet, HelmetProvider } from 'react-helmet-async';
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <HelmetProvider>
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-//   </HelmetProvider>
-// );
-
 
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
@@ -22,15 +5,32 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
 
+import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div> 
-      Indrojit Mondal
+    element: 
+       <div> 
+     
 
-      <p className="text-red-500">Hello....</p>
+      <p className="text-red-500">Hello from main.jsx....</p>
+        
+      <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+      
       </div>,
   },
 ]);
